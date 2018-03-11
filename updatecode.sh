ba19266c1ff35a63811fdf8257cf34f49ed1477e
jbmadeleine <jean-baptiste.madeleine@upmc.fr>
2018-03-11 16:38:58 +0100

    Added an update script

---
+++ updatecode.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+echo "*** record local changes"
+#git add INIT/planet_start
+#git add RUN/etu.def
+git commit -m "save local"
+
+echo "*** update eduplanet"
+git pull
+
+#echo "*** update LMD models"
+#cd MODELES
+#svn update -r 1370
+#cd ..
+#
+#echo "*** update planetoplot"
+#cd TOOLS/planetoplot
+#git pull
+#cd ../..
+#
+#echo "*** update planets"
+#cd TOOLS/planets
+#git pull
+#cd ../..
+#
+#echo "*** get supplementary files"
+#cd RUN/DATAGENERIC
+#if [[ ! (-f "surface_earth.nc") ]] ; then
+#  wget "http://data.spiga.fr/eduplanet/surface_earth.nc"
+#fi
+#if [[ ! (-f "surface_mars.nc") ]] ; then
+#  wget "http://data.spiga.fr/eduplanet/surface_mars.nc"
+#fi
+#cd ../..
